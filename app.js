9067b53f381191346ad3ae1e9cbf7ef5ab5edabd
mahume <michael.a.hume@gmail.com>
2020-01-12 20:27:45 -0700

    Adding req.body parsing middleware

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const routes = require('./routes');
 
 const app = express();
 
+// MIDDLEWARE
+// Parse req.body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
 app.use('/', routes);
 
 module.exports = app;
